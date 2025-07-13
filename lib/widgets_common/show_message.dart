11c6d9c7ba700b798ee5fd82eab52d8876091444
FuturisticGoo <amananiferfiaff@gmail.com>
2025-07-13 11:33:05 +0530

    Stop snackbar from queing when copying, instead show immediately

--- lib/widgets_common/show_message.dart
+++ lib/widgets_common/show_message.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-void showSnackBar(BuildContext context, {required String text}) {
-  ScaffoldMessenger.of(context).showSnackBar(
+void showSnackBar(BuildContext context, {required String text}) async {
+  ScaffoldMessenger.of(context).clearSnackBars();
+  final controller = ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(content: Text(text)),
   );
+  await Future.delayed(Durations.long4);
+  try {
+    controller.close();
+  } on AssertionError {
+    // Ignore, it happens when closing an already closed snackbar
+  }
 }
 
 void showAlertDialog(
